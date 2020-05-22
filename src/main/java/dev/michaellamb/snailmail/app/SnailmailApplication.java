9874addffa93ba8422db138057695804d96abe3f
mlamb <mlamb@cspire.com>
2020-05-21 22:22:55 -0500

    initial commit
    This application will support a website that captures and standardizes a mailing address.

---
+++ src/main/java/dev/michaellamb/snailmail/app/SnailmailApplication.java
@@ -0,0 +1,13 @@
+package dev.michaellamb.snailmail.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailmailApplication.class, args);
+	}
+
+}
